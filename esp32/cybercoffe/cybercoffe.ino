691a0a937eef117070d8d3f080e1ba8e93941c73
Renan Santana <renan_ota@hotmail.com>
2021-01-11 00:56:56 -0300

    Implementa pomodoro timer e buzzer

--- esp32/cybercoffe/cybercoffe.ino
+++ esp32/cybercoffe/cybercoffe.ino
@@ -13,9 +13,17 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+#define BUZZER_PIN 15
 
+const int channel = 0;
+const int frequence = 2000;
+const int resolution = 10;
+const int pomodoroWorkTime = 1500; // 25 minutes
+const int pomodoroShortBreak = 300; // 5 minutes
+const int pomodoroLongBreak = 600; // 10 minutes
+const int pomodoroRounds = 4;
 const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = -3600*3;
+const long gmtOffset_sec = -3600 * 3;
 const int daylightOffset_sec = 0;
 const int serialSpeed = 115200;
 const char* ssid = "ssid";
@@ -28,7 +36,19 @@ const char* mqttResource = "notification";
 
 String notification; // Armazenará a notificação atual sendo exibida
 char hour[6]; // Armazenará a hora atual
+int pomodoroTimer = pomodoroWorkTime; // Armazenará o timer do pomodoro, em segundos
+int pomodoroMinutes;
+int pomodoroSeconds;
+int pomodoroRound = 1; // Armazena o round do pomodoro, pois a cada 3 shortBreaks, vem 1 longBreak
+char pomodoroClock[5]; // Armazena o timer do pomodoro formatado "99:99"
 int  x, minX; // Usadas para movimentação horizontal de textos no display
+TaskHandle_t pomodoroTimerHandle; // Handler do pomodoroTimer, que rodará em outro núcleo
+eTaskState statusOf; // // Armazenará o status do pomodoroTimer, que rodará em outro núcleo
+int active_mode; // Usado para armazenar o modo ativo no momento (relógio/pomodoro/motor)
+// Modos disponíveis:
+// 1: Clock
+// 2: Pomodoro
+// 3: Motor
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -45,6 +65,11 @@ void setupWifi() {
   Serial.println();
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void setupBuzzer() {
+  ledcSetup(channel, frequence, resolution);
+  ledcAttachPin(BUZZER_PIN, channel);
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
@@ -129,17 +154,47 @@ void setupClock() {
 
 void getClockInfo() {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return;
   }
   strftime(hour, 6, "%H:%M", &timeinfo);
 }
 
+void secondsToMinuteSeconds(const int seconds, int &m, int &s) {
+  int t = seconds;
+  s = t % 60;
+  t = (t - s) / 60;
+  m = t % 60;
+  t = (t - m) / 60;
+}
+
+void startPomodoroTimer(void *pvParameters) {
+  while (pomodoroTimer > 0) {
+    pomodoroTimer -= 1;
+    secondsToMinuteSeconds(pomodoroTimer, pomodoroMinutes, pomodoroSeconds);
+    delay(1000);
+  }
+  // Tocar o alarme ao fim do tempo
+  //ledcWriteTone(channel, 2000);
+  //delay(1000);
+  //ledcWriteTone(channel, 0);
+  pomodoroRound += 1;
+  pomodoroTimer = pomodoroWorkTime;
+  vTaskDelete(NULL);
+}
+
 void showClock() {
   display.setTextSize(3);
   display.setCursor(20, 24);
   display.println(hour);
+}
+
+void showPomodoroTimer() {
+  display.setTextSize(3);
+  display.setCursor(20, 24);
+  sprintf(pomodoroClock, "%02d:%02d", pomodoroMinutes, pomodoroSeconds);
+  display.println(pomodoroClock);
 }
 
 void setScrollingMessage(String message) {
@@ -159,21 +214,38 @@ void scrollMessage() {
 }
 
 void setup() {
+  active_mode = 1;
   Serial.begin(serialSpeed);
   setupWifi();
+  //setupBuzzer();
   setupMQTT();
   setupDisplay();
   mqttPublish(mqttResource, "ESP32 initialized", false);
   setupClock();
   getClockInfo();
   showClock();
+  // xTaskCreatePinnedToCore(startPomodoroTimer, "startPomodoroTimer", 10000, NULL, 1, &pomodoroTimerHandle, 0);
 }
 
 void loop() {
   client.loop();
-  getClockInfo();
   display.clearDisplay();
+  if (active_mode == 1) {
+    // Modo relógio
+    getClockInfo();
+    showClock();
+  } else if (active_mode == 2) {
+    // Modo pomodoro timer
+    showPomodoroTimer();
+    statusOf = eTaskGetState(pomodoroTimerHandle);
+    // Verifica se a task pomodoro já terminou
+    if (statusOf == eReady) {
+      active_mode = 1;
+    }
+  } else if (active_mode == 3) {
+    // Modo rotor
+
+  }
   scrollMessage();
-  showClock();
   display.display();
 }
