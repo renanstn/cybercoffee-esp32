ce33cef77d8588f0dac4823e13320c8a71387f94
Renan Santana <renan_ota@hotmail.com>
2021-01-11 12:53:53 -0300

    Enpoint de alteração de modo

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import logging
 from settings import BEEBOTTE_API_KEY, BEEBOTTE_SECRET_KEY
-from model import NotificationModel
+from model import NotificationModel, SetModeModel
 from fastapi import FastAPI
 from beebotte import BBT, Resource
 from unidecode import unidecode
@@ -24,5 +24,16 @@ async def notify(notification: NotificationModel):
         resource = Resource(beebotte_client, "esp32", "notification")
         resource.publish(message)
         return {'message': 'Notification sent!'}
+    except:
+        return {'message': 'Error!'}
+
+@app.post('/set-mode/')
+async def set_mode(set_mode: SetModeModel):
+    try:
+        mode = set_mode.mode
+        beebotte_client = BBT(BEEBOTTE_API_KEY, BEEBOTTE_SECRET_KEY)
+        resource = Resource(beebotte_client, "esp32", "mode")
+        resource.publish(mode)
+        return {'message': 'Mode changed!'}
     except:
         return {'message': 'Error!'}
--- server/model.py
+++ server/model.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class NotificationModel(BaseModel):
     sender: str
     message: str
+
+
+class SetModeModel(BaseModel):
+    mode: str
